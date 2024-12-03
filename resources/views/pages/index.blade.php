20bf0b2af9b6fb6642f49fb7aa7f49b50b41baa5
DanniAdhyatma <dannirachman2@gmail.com>
2024-12-03 09:09:39 +0800

    Update landing page ponpes

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -38,10 +38,13 @@
             <!-- Menu -->
             <div
                 x-bind:class="{ 'hidden': !open }"
-                class="w-full md:block md:w-auto"
+                class="w-full md:block md:w-auto px-1 !important"
                 id="navbar-default"
                 x-cloak>
                 <ul class="flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 md:mt-0">
+                    <li>
+                        <a href="#" class="block py-2 px-3" aria-current="page">Beranda</a>
+                    </li>
                     <li>
                         <a href="#" class="block py-2 px-3" aria-current="page">Tentang Al-Mazaya</a>
                     </li>
@@ -55,70 +58,159 @@
             </div>
         </div>
     </nav>
-
-
-    <section class="hero-section bg-green-800 text-white">
-        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+    <!-- Hero section -->
+    <section class="hero-section py-20 bg-green-800 text-white">
+        <div class="grid max-w-screen-xl px-0 py-0 mx-auto lg:gap-8 xl:gap-0 lg:py-8 lg:grid-cols-12">
             <div class="mr-auto place-self-center lg:col-span-7">
+                <div class="flex items-center justify-left">
+                    <p class="max-w-2xl mb-2 font-light border rounded-2xl border-white px-2 py-2 lg:mb-2 md:text-lg lg:text-xl">Pendaftaran Dibuka</p>
+                </div>
                 <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-5xl dark:text-white">Selamat Datang di Pondok Pesantren Al Mazaya</h1>
-                <p class="max-w-2xl mb-6 font-light  lg:mb-8 md:text-lg lg:text-xl ">Membentuk generasi Muslim yang berilmu, berakhlak mulia, dan siap menghadapi tantangan global.</p>
-                <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center border rounded-lg bg-white text-black hover:text-white hover:bg-green-600 ">
+                <p class="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl">Membentuk generasi Muslim yang berilmu, berakhlak mulia, dan siap menghadapi tantangan global.</p>
+                <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center border rounded-lg bg-white text-black hover:text-white hover:bg-green-600">
                     Daftar Sekarang
                 </a>
             </div>
-            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup">
-            </div>
+
+            <!-- <div class="hidden lg:mt-4 lg:col-span-5 lg:flex">
+                <img src="{{ asset('images/school.png') }}"
+                    alt="ponpes-al-mazaya-highlight"
+                    class="w-full max-w-md absolute right-0 pt-">
+            </div> -->
         </div>
     </section>
+    <!-- End hero section -->
+
+    <!-- Headline customer section -->
     <section class="highlight bg-white">
-        <div class="grid max-w-screen-xl px-4 py-16 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 my-32">
-            <!-- Kolom Teks -->
+        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-8 lg:grid-cols-12 my-32">
+            <!-- Title Headline -->
             <div class="mr-auto place-self-center lg:col-span-7">
-                <h2 class="text-3xl font-bold mb-4">Headline highlighting customer results</h2>
+                <h2 class="text-5xl font-bold mb-4">Headline highlighting customer results</h2>
                 <p class="text-lg mb-8">Highlight the Unique Selling Proposition (USP) with a short summary of the main feature and how it benefits customers. The idea here is to keep it short and direct. If the visitor wishes to learn more they will hit the button.</p>
 
-                <div class="grid grid-cols-2 gap-8">
-                    <div class="text-center bg-green-100 p-8 rounded-lg shadow-md">
-                        <h3 class="text-2xl font-bold mb-2">527</h3>
+                <div class="grid grid-cols-2 gap-4">
+                    <div class="text-start  p-4  border-l-4 border-green-500">
+                        <p class="font-bold mb-2 text-5xl">527</p>
                         <p>Jumlah siswa</p>
                     </div>
-                    <div class="text-center bg-green-100 p-8 rounded-lg shadow-md">
-                        <h3 class="text-2xl font-bold mb-2">4236</h3>
+                    <div class="text-start  p-4  border-l-4 border-green-500">
+                        <p class="font-bold mb-2 text-5xl">4236</p>
                         <p>Jumlah alumni</p>
                     </div>
                 </div>
             </div>
-            <!-- Kolom Gambar -->
-            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                <img src="{{ asset('images/highlight.svg') }}" alt="ponpes-al-mazaya-highlight" class="w-full max-w-md rounded-lg">
+
+            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex pl-14">
+                <img src="{{ asset('images/ponpes-headline.png') }}" alt="ponpes-al-mazaya-highlight" class="w-full max-w-lg lg:max-w-xl">
             </div>
         </div>
     </section>
-    <section class="profile bg-green-800">
-        <div class="grid max-w-screen-xl px-4 py-16 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 my-32">
-            <!-- Kolom Teks -->
-            <div class="mr-auto place-self-center lg:col-span-7">
-                <h2 class="text-3xl font-bold mb-4">Guru dan Staff Al Mazaya</h2>
-                <p class="text-lg mb-8">Highlight the Unique Selling Proposition (USP) with a short summary of the main feature and how it benefits customers. The idea here is to keep it short and direct. If the visitor wishes to learn more they will hit the button.</p>
 
-                <div class="grid grid-cols-2 gap-8">
-                    <div class="text-center bg-green-100 p-8 rounded-lg shadow-md">
-                        <h3 class="text-2xl font-bold mb-2">527</h3>
-                        <p>Jumlah siswa</p>
+    <!-- Guru dan staff -->
+    <section class="staff bg-green-800">
+        <div class="grid max-w-screen-xl px-4 py-16 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 my-32">
+            <!-- Title Guru dan staff -->
+            <div class="mr-auto place-self-center lg:col-span-12 text-center">
+                <h2 class="text-3xl font-bold mb-4 text-white">Guru dan Staff Al Mazaya</h2>
+                <p class="text-sm mb-8 text-white">
+                    Highlight the Unique Selling Proposition (USP) with a short summary of the main feature and how it benefits customers. The idea here is to keep it short and direct. If the visitor wishes to learn more they will hit the button.
+                </p>
+            </div>
+
+            <!-- Cards Guru -->
+            <div class="lg:col-span-12">
+                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 justify-center">
+
+                    <div class="text-start bg-gray-200 p-8 rounded-lg shadow-md">
+                        <img src="{{ asset('images/asset-berita.png') }}" alt="ponpes-al-mazaya-highlight" class="w-full max-w-md rounded-md mb-6">
+                        <div class="border-white p-2 rounded-lg bg-white">
+                            <h3 class="text-xl font-bold mb-2">Alifudin Rahman</h3>
+                            <p>Guru Tajwid</p>
+                        </div>
                     </div>
-                    <div class="text-center bg-green-100 p-8 rounded-lg shadow-md">
-                        <h3 class="text-2xl font-bold mb-2">4236</h3>
-                        <p>Jumlah alumni</p>
+
+                    <div class="text-start bg-gray-200 p-8 rounded-lg shadow-md">
+                        <img src="{{ asset('images/asset-berita.png') }}" alt="ponpes-al-mazaya-highlight" class="w-full max-w-md rounded-md mb-6">
+                        <div class="border border-white p-2 rounded-lg bg-white">
+                            <h3 class="text-xl font-bold mb-2">Alifudin Rahman</h3>
+                            <p>Guru Fiqih</p>
+                        </div>
+                    </div>
+
+                    <div class="text-start bg-gray-200 p-8 rounded-lg shadow-md">
+                        <img src="{{ asset('images/asset-berita.png') }}" alt="ponpes-al-mazaya-highlight" class="w-full max-w-md rounded-md mb-6">
+                        <div class="border border-white p-2 rounded-lg bg-white">
+                            <h3 class="text-xl font-bold mb-2">Alifudin Rahman</h3>
+                            <p>Guru Akidah</p>
+                        </div>
+                    </div>
+
+                    <div class="text-start bg-gray-200 p-8 rounded-lg shadow-md">
+                        <img src="{{ asset('images/asset-berita.png') }}" alt="ponpes-al-mazaya-highlight" class="w-full max-w-md rounded-md mb-6">
+                        <div class="border-white p-2 rounded-lg bg-white">
+                            <h3 class="text-xl font-bold mb-2">Alifudin Rahman</h3>
+                            <p>Guru Akhlak</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <!-- Kolom Gambar -->
-            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                <img src="{{ asset('images/highlight.svg') }}" alt="ponpes-al-mazaya-highlight" class="w-full max-w-md rounded-lg">
-            </div>
         </div>
     </section>
+
+    <!-- End section guru dan staff -->
+    <!-- Berita al-mazaya -->
+    <section class="news">
+        <div class="flex flex-col justify-center items-center max-w-screen-xl px-4 py-8 mx-auto text-center">
+            <!-- Kolom Teks -->
+            <div class="lg:w-3/4">
+                <h2 class="text-3xl font-bold mb-4 text-black">Berita Al Mazaya</h2>
+                <p class="text-lg mb-8 text-black">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio corporis, molestiae in aperiam debitis dignissimos excepturi ipsam fuga pariatur sint eum recusandae nesciunt, sequi iure perspiciatis placeat, voluptatem cupiditate.
+                </p>
+            </div>
+
+            <!-- Card section -->
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-8 w-full lg:w-3/4">
+                <!-- Card 1 -->
+                <div class="text-start p-4 rounded-lg">
+                    <img src="{{ asset('images/asset-berita.png') }}" alt="ponpes-al-mazaya-highlight" class="w-full max-w-md rounded-md">
+                    <h3 class="text-xl font-bold mt-4 mb-2">Al Mazaya mengadakan kurban di lingkungan pondok</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et saepe doloribus veniam.</p>
+                </div>
+                <!-- Card 2 -->
+                <div class="text-start p-4 rounded-lg">
+                    <img src="{{ asset('images/asset-berita.png') }}" alt="ponpes-al-mazaya-highlight" class="w-full max-w-md rounded-md">
+                    <h3 class="text-xl font-bold mt-4 mb-2">Al Mazaya mengadakan kurban di lingkungan pondok</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et saepe doloribus veniam.</p>
+                </div>
+                <!-- Card 3 -->
+                <div class="text-start p-4 rounded-lg">
+                    <img src="{{ asset('images/asset-berita.png') }}" alt="ponpes-al-mazaya-highlight" class="w-full max-w-md rounded-md">
+                    <h3 class="text-xl font-bold mt-4 mb-2">Al Mazaya mengadakan kurban di lingkungan pondok</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et saepe doloribus veniam.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Tombol Berita Lainnya -->
+        <div class="flex justify-center mt-2 mb-10">
+            <a href="#" class="inline-block px-6 py-3 text-black font-medium border border-black rounded-lg hover:bg-black hover:text-white transition">
+                Berita Lainnya
+            </a>
+        </div>
+        </div>
+    </section>
+    <!-- End section berita al-mazaya -->
+
+    <!-- section video tentang al-mazaya -->
+    <section class="video">
+        <div class="flex flex-col justify-center items-center px-0 pt-16 pb-16 mx-auto text-center">
+            <h1 class="text-3xl font-bold mb-6 text-black">Video Tentang Al-Mazaya</h1>
+            <img src="{{ asset('images/placeholder-img.png') }}" alt="ponpes-al-mazaya-highlight" class="max-w-3xl w-full h-auto rounded-lg shadow-md">
+        </div>
+    </section>
+    <!-- End section video tentang al-mazaya -->
+
     <!-- footer -->
     <footer class="bg-green-900 text-white py-8">
         <div class="max-w-screen-xl mx-auto px-4">
