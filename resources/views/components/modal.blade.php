8704c1d321e1698c342554fc511e6cfe74d70c89
DanniAdhyatma <dannirachman2@gmail.com>
2024-12-21 06:57:04 +0800

    update code

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -18,10 +18,8 @@ $maxWidth = [
     x-data="{
         show: @js($show),
         focusables() {
-            // All focusable element types...
             let selector = 'a, button, input:not([type=\'hidden\']), textarea, select, details, [tabindex]:not([tabindex=\'-1\'])'
             return [...$el.querySelectorAll(selector)]
-                // All non-disabled elements...
                 .filter(el => ! el.hasAttribute('disabled'))
         },
         firstFocusable() { return this.focusables()[0] },
@@ -46,8 +44,8 @@ $maxWidth = [
     x-on:keydown.tab.prevent="$event.shiftKey || nextFocusable().focus()"
     x-on:keydown.shift.tab.prevent="prevFocusable().focus()"
     x-show="show"
-    class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50"
-    style="display: {{ $show ? 'block' : 'none' }};">
+    class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50">
+
     <div
         x-show="show"
         class="fixed inset-0 transform transition-all"
@@ -63,7 +61,7 @@ $maxWidth = [
 
     <div
         x-show="show"
-        class="mb-6 bg-white  rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
+        class="mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
         x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
