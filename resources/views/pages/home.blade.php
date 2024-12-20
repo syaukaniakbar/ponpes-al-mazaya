e21b6311aaa1c919f5790764712f55dd0d854218
DanniAdhyatma <dannirachman2@gmail.com>
2024-12-21 00:58:15 +0800

    update code

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,6 @@
 <x-main-layout>
 
     @section('title', 'Home | Ponpes-Al-Mazaya') <!-- Mengisi bagian @yield('title') di parent -->
-
     <section class="hero-section bg-green-800 text-white">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             <div class="lg:col-span-12">
