950804b303251476ccf5f2ac9089aa3f4c358a45
DanniAdhyatma <dannirachman2@gmail.com>
2024-12-21 00:55:43 +0800

    update code

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -18,23 +18,22 @@ $maxWidth = [
     x-data="{
         show: @js($show),
         focusables() {
-            // All focusable element types...
-            let selector = 'a, button, input:not([type=\'hidden\']), textarea, select, details, [tabindex]:not([tabindex=\'-1\'])'
-            return [...$el.querySelectorAll(selector)]
-                // All non-disabled elements...
-                .filter(el => ! el.hasAttribute('disabled'))
+            let selector = 'a, button, input:not([type=\'hidden\']), textarea, select, details, [tabindex]:not([tabindex=\'-1\'])';
+            return [...$el.querySelectorAll(selector)].filter(el => !el.hasAttribute('disabled'));
         },
-        firstFocusable() { return this.focusables()[0] },
-        lastFocusable() { return this.focusables().slice(-1)[0] },
-        nextFocusable() { return this.focusables()[this.nextFocusableIndex()] || this.firstFocusable() },
-        prevFocusable() { return this.focusables()[this.prevFocusableIndex()] || this.lastFocusable() },
-        nextFocusableIndex() { return (this.focusables().indexOf(document.activeElement) + 1) % (this.focusables().length + 1) },
-        prevFocusableIndex() { return Math.max(0, this.focusables().indexOf(document.activeElement)) -1 },
+        firstFocusable() { return this.focusables()[0]; },
+        lastFocusable() { return this.focusables().slice(-1)[0]; },
+        nextFocusable() { return this.focusables()[this.nextFocusableIndex()] || this.firstFocusable(); },
+        prevFocusable() { return this.focusables()[this.prevFocusableIndex()] || this.lastFocusable(); },
+        nextFocusableIndex() { return (this.focusables().indexOf(document.activeElement) + 1) % this.focusables().length; },
+        prevFocusableIndex() { return Math.max(0, this.focusables().indexOf(document.activeElement) - 1); },
     }"
     x-init="$watch('show', value => {
         if (value) {
             document.body.classList.add('overflow-y-hidden');
-            {{ $attributes->has('focusable') ? 'setTimeout(() => firstFocusable().focus(), 100)' : '' }}
+            @if ($attributes->has('focusable'))
+                setTimeout(() => firstFocusable()?.focus(), 100);
+            @endif
         } else {
             document.body.classList.remove('overflow-y-hidden');
         }
@@ -47,7 +46,7 @@ $maxWidth = [
     x-on:keydown.shift.tab.prevent="prevFocusable().focus()"
     x-show="show"
     class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50"
-    style="display: {{ $show ? 'block' : 'none' }};">
+    :style="show ? 'display: block;' : 'display: none;'">
     <div
         x-show="show"
         class="fixed inset-0 transform transition-all"
@@ -63,7 +62,7 @@ $maxWidth = [
 
     <div
         x-show="show"
-        class="mb-6 bg-white  rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
+        class="mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
         x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
