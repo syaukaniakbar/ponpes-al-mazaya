62afa01a16d5ccf5238b6c53e736800ae394b989
DanniAdhyatma <dannirachman2@gmail.com>
2024-12-21 09:41:31 +0800

    Update config for overflow

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,27 @@ export default {
         "./node_modules/flowbite/**/*.js",
     ],
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                'xs': '375px',
+                'sm': '640px',
+                'md': '768px',
+                'lg': '1024px',
+                'xl': '1280px',
+                '2xl': '1536px',
+            },
+            container: {
+                center: true,
+                padding: {
+                    DEFAULT: '1rem',
+                    sm: '2rem',
+                    md: '3rem',
+                    lg: '4rem',
+                    xl: '5rem',
+                    '2xl': '6rem',
+                },
+            },
+        },
     },
     plugins: [
         require("flowbite-typography"),
@@ -14,4 +34,13 @@ export default {
             wysiwyg: true,
         }),
     ],
+    variants: {
+        extend: {
+            overflow: ['responsive'],
+            display: ['responsive'],
+            padding: ['responsive'],
+            margin: ['responsive'],
+            width: ['responsive'],
+        }
+    },
 };
