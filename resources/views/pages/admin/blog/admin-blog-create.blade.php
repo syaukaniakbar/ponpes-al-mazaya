69d2b8216a1a56db9376d9b3e77ce440a7b13ccb
Snawlaye <farizfahrian53@gmail.com>
2024-12-29 16:18:19 +0800

    nothing changed

--- resources/views/pages/admin/blog/admin-blog-create.blade.php
+++ resources/views/pages/admin/blog/admin-blog-create.blade.php
@@ -1,5 +1,5 @@
 <x-dashboard-layout>
-    
+
     <style>
         @import url(https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.min.css);
 
@@ -19,33 +19,33 @@
         }
     </style>
 
-    <div class="max-w-2xl mx-auto p-6 bg-white shadow-md rounded-md">
-        <h2 class="text-2xl font-bold text-gray-800 mb-6">Buat Postingan</h2>
+    <div class="max-w-2xl p-6 mx-auto bg-white rounded-md shadow-md">
+        <h2 class="mb-6 text-2xl font-bold text-gray-800">Buat Postingan</h2>
         @if(session('success'))
-            <div x-data="{ show: true }"
-                x-init="setTimeout(() => show = false, 3000)"
-                x-show="show"
-                x-transition:enter="transition ease-out duration-300"
-                x-transition:enter-start="opacity-0 transform scale-90"
-                x-transition:enter-end="opacity-100 transform scale-100"
-                x-transition:leave="transition ease-in duration-300"
-                x-transition:leave-start="opacity-100 transform scale-100"
-                x-transition:leave-end="opacity-0 transform scale-90"
-                class="fixed top-24 right-5 max-w-xs w-full p-4 text-sm text-green-800 bg-green-100 border border-green-300 rounded-md shadow-lg flex items-center space-x-3 z-50"
-                role="alert">
-                <!-- Icon -->
-                <svg class="w-5 h-5 text-green-800" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 00-1.414 0L8 12.586 4.707 9.293a1 1 0 10-1.414 1.414l4 4a1 1 0 001.414 0l8-8a1 1 0 000-1.414z" clip-rule="evenodd" />
-                </svg>
-                <!-- Message -->
-                <span>{{ session('success') }}</span>
-            </div>
+        <div x-data="{ show: true }"
+            x-init="setTimeout(() => show = false, 3000)"
+            x-show="show"
+            x-transition:enter="transition ease-out duration-300"
+            x-transition:enter-start="opacity-0 transform scale-90"
+            x-transition:enter-end="opacity-100 transform scale-100"
+            x-transition:leave="transition ease-in duration-300"
+            x-transition:leave-start="opacity-100 transform scale-100"
+            x-transition:leave-end="opacity-0 transform scale-90"
+            class="fixed z-50 flex items-center w-full max-w-xs p-4 space-x-3 text-sm text-green-800 bg-green-100 border border-green-300 rounded-md shadow-lg top-24 right-5"
+            role="alert">
+            <!-- Icon -->
+            <svg class="w-5 h-5 text-green-800" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 00-1.414 0L8 12.586 4.707 9.293a1 1 0 10-1.414 1.414l4 4a1 1 0 001.414 0l8-8a1 1 0 000-1.414z" clip-rule="evenodd" />
+            </svg>
+            <!-- Message -->
+            <span>{{ session('success') }}</span>
+        </div>
         @endif
 
-        <form action="{{ route('blog.store') }}" method="POST" enctype="multipart/form-data"  onsubmit="submitForm()">
+        <form action="{{ route('blog.store') }}" method="POST" enctype="multipart/form-data" onsubmit="submitForm()">
             @csrf
             <div class="my-6">
-                <label for="title" class="block text-lg font-medium text-gray-800 mb-2">Title</label>
+                <label for="title" class="block mb-2 text-lg font-medium text-gray-800">Title</label>
                 <input
                     type="text"
                     id="title"
@@ -54,7 +54,7 @@
                     placeholder="Masukkan Judul"
                     value="{{ old('title')}}"
                     maxlength="100">
-                    
+
             </div>
             <p>Jumlah Karakter <span class="jumlah">0</span></p>
             <fieldset>
@@ -66,7 +66,7 @@
                         value="prestasi"
                         class="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300"
                         {{ old('category') == 'prestasi' ? 'checked' : '' }}>
-                    <label for="prestasi" class="block ms-2 text-sm font-medium text-gray-900">Prestasi</label>
+                    <label for="prestasi" class="block text-sm font-medium text-gray-900 ms-2">Prestasi</label>
                 </div>
                 <div class="flex items-center mb-4">
                     <input
@@ -76,7 +76,7 @@
                         value="umum"
                         class="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300"
                         {{ old('category') == 'umum' ? 'checked' : '' }}>
-                    <label for="umum" class="block ms-2 text-sm font-medium text-gray-900">Umum</label>
+                    <label for="umum" class="block text-sm font-medium text-gray-900 ms-2">Umum</label>
                 </div>
                 <div class="flex items-center mb-4">
                     <input
@@ -86,7 +86,7 @@
                         value="ilmiah"
                         class="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300"
                         {{ old('category') == 'ilmiah' ? 'checked' : '' }}>
-                    <label for="ilmiah" class="block ms-2 text-sm font-medium text-gray-900">Ilmiah</label>
+                    <label for="ilmiah" class="block text-sm font-medium text-gray-900 ms-2">Ilmiah</label>
                 </div>
             </fieldset>
 
@@ -121,7 +121,7 @@
                                 <i class="mdi mdi-image"></i>
                             </button>
                             <input type="file" id="imageUpload" class="hidden" accept="image/*" @change="insertImageFromFile">
-                                                     
+
                             <button type="button" class="w-10 h-10 border-r border-gray-200 outline-none focus:outline-none hover:text-indigo-500 active:bg-gray-50" @click="format('formatBlock','H2')">
                                 <i class="mdi mdi-format-header-2"></i>
                             </button>
@@ -163,31 +163,30 @@
                 }
                  } }" class="space-y-4">
                 <!-- File Input -->
-                <input 
-                    class="block w-full text-2xl text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-100 focus:outline-none "
-                    id="image" 
-                    type="file" 
-                    name="image" 
+                <input
+                    class="block w-full text-2xl text-gray-900 bg-gray-100 border border-gray-300 rounded-lg cursor-pointer focus:outline-none "
+                    id="image"
+                    type="file"
+                    name="image"
                     accept="image/*"
-                    @change="handleFilePreview($event)" 
-                >
-                
+                    @change="handleFilePreview($event)">
+
                 <!-- Image Preview -->
                 <template x-if="imagePreview">
                     <div class="w-full max-w-sm mx-auto">
-                        <img :src="imagePreview" alt="Selected Image" class="w-full h-64 object-cover rounded-lg shadow-lg border-4 border-gray-200">
+                        <img :src="imagePreview" alt="Selected Image" class="object-cover w-full h-64 border-4 border-gray-200 rounded-lg shadow-lg">
                     </div>
                 </template>
             </div>
-               
-            
+
+
 
             <div class="flex justify-start">
-                <button type="button" class="w-full bg-blue-500 text-white rounded-md mt-4 px-4 py-2 hover:bg-blue-700 transition" onclick="openModal('modelConfirm')">
+                <button type="button" class="w-full px-4 py-2 mt-4 text-white transition bg-blue-500 rounded-md hover:bg-blue-700" onclick="openModal('modelConfirm')">
                     Simpan
                 </button>
-                <div id="modelConfirm" class="fixed hidden z-50 inset-0 bg-gray-900 bg-opacity-60 overflow-y-auto h-full w-full px-4 ">
-                    <div class="relative top-40 mx-auto shadow-xl rounded-md bg-white max-w-md">
+                <div id="modelConfirm" class="fixed inset-0 z-50 hidden w-full h-full px-4 overflow-y-auto bg-gray-900 bg-opacity-60 ">
+                    <div class="relative max-w-md mx-auto bg-white rounded-md shadow-xl top-40">
                         <div class="flex justify-end p-2">
                             <button onclick="closeModal('modelConfirm')" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center">
                                 <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -196,11 +195,11 @@
                             </button>
                         </div>
                         <div class="p-6 pt-0 text-center">
-                            <h3 class="text-xl font-normal text-gray-500 mt-5 mb-6">Lanjutkan Membuat Blog?</h3>
-                            <!-- Tombol submit -->
-                            <button type="submit" class="text-white bg-green-600 hover:bg-green-800 rounded-lg px-3 py-2.5">Ya, Lanjutkan</button>
+                            <h3 class="mt-5 mb-6 text-xl font-normal text-gray-500">Lanjutkan Membuat Blog?</h3>
                             <!-- Tombol batal -->
                             <button type="button" onclick="closeModal('modelConfirm')" class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 rounded-lg px-3 py-2.5">Tidak, Kembali</button>
+                            <!-- Tombol submit -->
+                            <button type="submit" class="text-white bg-green-600 hover:bg-green-800 rounded-lg px-3 py-2.5">Ya, Lanjutkan</button>
                         </div>
                     </div>
                 </div>
@@ -211,7 +210,7 @@
             <ul>
                 @foreach ($errors->all() as $error)
                 <li
-                    class="text-center p-4 mt-4 mb-4 text-sm text-white bg-red-600 border border-green-300 rounded-md">
+                    class="p-4 mt-4 mb-4 text-sm text-center text-white bg-red-600 border border-green-300 rounded-md">
                     {{ $error }}
                 </li>
                 @endforeach
@@ -219,8 +218,8 @@
         </div>
         @endif
     </div>
-    
-  
+
+
     {{-- Rich Editor --}}
     <script>
         function app() {
@@ -263,11 +262,11 @@
                         reader.onload = (e) => {
                             const img = new Image();
                             img.src = e.target.result;
-    
+
                             // Apply max-width and max-height to prevent oversized images
-                            img.style.maxWidth = "100%";  // Prevent image from overflowing the editor
+                            img.style.maxWidth = "100%"; // Prevent image from overflowing the editor
                             img.style.maxHeight = "400px"; // Adjust this value based on your preference
-    
+
                             // Insert the image at the current selection in the WYSIWYG editor
                             const iframeDoc = this.wysiwyg.contentDocument;
                             iframeDoc.body.appendChild(img);
@@ -292,7 +291,7 @@
             document.getElementById('description').value = wysiwygContent;
         }
     </script>
-    
+
 
 
     {{-- Menghitung jumlah karakter title --}}
@@ -300,13 +299,13 @@
         // Get the elements
         const titleInput = document.querySelector('#title');
         const jumlahSpan = document.querySelector('.jumlah');
-        
+
         // Update character count in real-time
         titleInput.addEventListener('input', function() {
             jumlahSpan.textContent = titleInput.value.length;
         });
     </script>
-     {{-- Akhir menghitung jumlah karakter title --}}
+    {{-- Akhir menghitung jumlah karakter title --}}
 
 
     {{-- Modal Button Simpan --}}
@@ -334,7 +333,7 @@
         };
     </script>
     {{-- Akhir Modal Button Simpan --}}
- 
+
 
 
 </x-dashboard-layout>
--- resources/views/pages/admin/blog/admin-blog.blade.php
+++ resources/views/pages/admin/blog/admin-blog.blade.php
@@ -5,84 +5,84 @@
 
     <div class="w-full mt-12">
         <a href="{{ route('blog.create') }}">
-            <button class="bg-green-600 text-white p-3 rounded">
+            <button class="p-3 text-white bg-green-600 rounded">
                 Tambah Blog
             </button>
         </a>
         @if(session('success'))
-            <div x-data="{ show: true }"
-                x-init="setTimeout(() => show = false, 3000)"
-                x-show="show"
-                x-transition:enter="transition ease-out duration-300"
-                x-transition:enter-start="opacity-0 transform scale-90"
-                x-transition:enter-end="opacity-100 transform scale-100"
-                x-transition:leave="transition ease-in duration-300"
-                x-transition:leave-start="opacity-100 transform scale-100"
-                x-transition:leave-end="opacity-0 transform scale-90"
-                class="fixed top-24 right-5 max-w-xs w-full p-4 text-sm text-green-800 bg-green-100 border border-green-300 rounded-md shadow-lg flex items-center space-x-3 z-50"
-                role="alert">
-                <!-- Icon -->
-                <svg class="w-5 h-5 text-green-800" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 00-1.414 0L8 12.586 4.707 9.293a1 1 0 10-1.414 1.414l4 4a1 1 0 001.414 0l8-8a1 1 0 000-1.414z" clip-rule="evenodd" />
-                </svg>
-                <!-- Message -->
-                <span>{{ session('success') }}</span>
-            </div>
+        <div x-data="{ show: true }"
+            x-init="setTimeout(() => show = false, 3000)"
+            x-show="show"
+            x-transition:enter="transition ease-out duration-300"
+            x-transition:enter-start="opacity-0 transform scale-90"
+            x-transition:enter-end="opacity-100 transform scale-100"
+            x-transition:leave="transition ease-in duration-300"
+            x-transition:leave-start="opacity-100 transform scale-100"
+            x-transition:leave-end="opacity-0 transform scale-90"
+            class="fixed z-50 flex items-center w-full max-w-xs p-4 space-x-3 text-sm text-green-800 bg-green-100 border border-green-300 rounded-md shadow-lg top-24 right-5"
+            role="alert">
+            <!-- Icon -->
+            <svg class="w-5 h-5 text-green-800" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 00-1.414 0L8 12.586 4.707 9.293a1 1 0 10-1.414 1.414l4 4a1 1 0 001.414 0l8-8a1 1 0 000-1.414z" clip-rule="evenodd" />
+            </svg>
+            <!-- Message -->
+            <span>{{ session('success') }}</span>
+        </div>
         @endif
-        <p class=" text-xl p-5 flex items-center">
+        <p class="flex items-center p-5 text-xl ">
             Al-Mazaya Blog
         </p>
-        <div class="bg-white overflow-auto">
-            <table class="min-w-full table-auto bg-white">
+        <div class="overflow-auto bg-white">
+            <table class="min-w-full bg-white table-auto">
                 <thead>
-                    <tr class="bg-gray-800 text-white">
-                        <th class="py-3 px-4 text-left text-sm font-semibold uppercase">No</th>
-                        <th class="py-3 px-4 text-left text-sm font-semibold uppercase">Image</th>
-                        <th class="py-3 px-4 text-left text-sm font-semibold uppercase">Title</th>
-                        <th class="py-3 px-4 text-left text-sm font-semibold uppercase">Description</th>
-                        <th class="py-3 px-4 text-left text-sm font-semibold uppercase">Category</th>
-                        <th class="py-3 px-4 text-left text-sm font-semibold uppercase">Date</th>
-                        <th class="py-3 px-4 text-left text-sm font-semibold uppercase">Action</th>
+                    <tr class="text-white bg-gray-800">
+                        <th class="px-4 py-3 text-sm font-semibold text-left uppercase">No</th>
+                        <th class="px-4 py-3 text-sm font-semibold text-left uppercase">Image</th>
+                        <th class="px-4 py-3 text-sm font-semibold text-left uppercase">Title</th>
+                        <th class="px-4 py-3 text-sm font-semibold text-left uppercase">Description</th>
+                        <th class="px-4 py-3 text-sm font-semibold text-left uppercase">Category</th>
+                        <th class="px-4 py-3 text-sm font-semibold text-left uppercase">Date</th>
+                        <th class="px-4 py-3 text-sm font-semibold text-left uppercase">Action</th>
                     </tr>
                 </thead>
-                <tbody>     
+                <tbody>
                     @forelse ($blogs as $key => $blog)
                     <tr class="border-b">
-                        <td class="py-3 px-4">{{ $blogs->firstItem() + $key }}</td>
+                        <td class="px-4 py-3">{{ $blogs->firstItem() + $key }}</td>
                         @if ($blog->image_url)
-                            <td class="py-3 px-4">
-                                <img src="{{ asset('storage/' . $blog->image_url) }}" alt="Blog Image" class="w-16 h-16 object-cover">
-                                <p>{{ asset('storage/' . $blog->image_url) }}</p>
-                            </td>
+                        <td class="px-4 py-3">
+                            <img src="{{ asset('storage/' . $blog->image_url) }}" alt="Blog Image" class="object-cover w-16 h-16">
+                            <!-- <p>{{ asset('storage/' . $blog->image_url) }}</p> -->
+                        </td>
                         @endif
-                        <td class="py-3 px-4">{{ $blog->title }}</td>
-                        <td class="py-3 px-4">{{ Str::limit($blog->description, 100) }}</td>
-                        <td class="py-3 px-4">{{ ucfirst($blog->category) }}</td>
-                        <td class="py-3 px-4">{{ $blog->created_at->format('d/m/Y') }}</td>
-                        <td class="py-3 px-4">
-                            <button onclick="openModal({{ $blog->id }})" class="text-white w-full bg-blue-600 rounded p-2 mb-2 text-center block">
+                        <td class="px-4 py-3">{{ $blog->title }}</td>
+                        <td class="px-4 py-3">{{ Str::limit($blog->description, 100) }}</td>
+                        <td class="px-4 py-3">{{ ucfirst($blog->category) }}</td>
+                        <td class="px-4 py-3">{{ $blog->created_at->format('d/m/Y') }}</td>
+                        <td class="px-4 py-3">
+                            <button onclick="openModal({{ $blog->id }})" class="block w-full p-2 mb-2 text-center text-white bg-blue-600 rounded">
                                 Detail
                             </button>
-                            <a href="{{ route('blog.edit', $blog->id) }}" class="text-white w-full bg-blue-600 rounded p-2 mb-2 text-center block">
+                            <a href="{{ route('blog.edit', $blog->id) }}" class="block w-full p-2 mb-2 text-center text-white bg-blue-600 rounded">
                                 Edit
                             </a>
                             <form action="{{ route('blog.delete', $blog->id) }}" method="post">
                                 @csrf
                                 @method('delete')
-                                <button type="submit" class="text-white w-full bg-red-600 rounded p-2">Delete</button>
+                                <button type="submit" class="w-full p-2 text-white bg-red-600 rounded">Delete</button>
                             </form>
                         </td>
                     </tr>
                     @empty
                     <tr>
-                        <td colspan="7" class="text-center items-center py-10">No records found!</td>
+                        <td colspan="7" class="items-center py-10 text-center">No records found!</td>
                     </tr>
                     @endforelse
-                      <!-- Pagination -->
+                    <!-- Pagination -->
                 </tbody>
             </table>
             <!-- Pagination -->
-            <div class="mt-6 p-10 flex justify-center">
+            <div class="flex justify-center p-10 mt-6">
                 <div class="flex items-center space-x-2">
                     {{ $blogs->links('pagination::tailwind') }}
                 </div>
@@ -98,7 +98,7 @@
             x-transition:leave="transition ease-in duration-300"
             x-transition:leave-start="opacity-100 transform scale-100"
             x-transition:leave-end="opacity-0 transform scale-90"
-            class="fixed top-24 right-5 max-w-xs w-full p-4 text-sm text-green-800 bg-green-100 border border-green-300 rounded-md shadow-lg flex items-center space-x-3 z-50"
+            class="fixed z-50 flex items-center w-full max-w-xs p-4 space-x-3 text-sm text-green-800 bg-green-100 border border-green-300 rounded-md shadow-lg top-24 right-5"
             role="alert">
             <!-- Icon -->
             <svg class="w-5 h-5 text-green-800" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -112,11 +112,11 @@
     </div>
 
     <!-- Modal for Showing Blog Details -->
-    <div id="blogDetailModal" class="fixed inset-0 bg-gray-900 bg-opacity-60 overflow-y-auto h-full w-full hidden">
-        <div class="relative top-20 mx-auto shadow-xl rounded-lg bg-white max-w-4xl">
+    <div id="blogDetailModal" class="fixed inset-0 hidden w-full h-full overflow-y-auto bg-gray-900 bg-opacity-60">
+        <div class="relative max-w-4xl mx-auto bg-white rounded-lg shadow-xl top-20">
             <!-- Close Button -->
             <div class="flex justify-end p-4">
-                <button onclick="closeModal()" class="text-gray-400 bg-transparent hover:bg-gray-200 rounded-lg text-sm p-2 inline-flex items-center">
+                <button onclick="closeModal()" class="inline-flex items-center p-2 text-sm text-gray-400 bg-transparent rounded-lg hover:bg-gray-200">
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                     </svg>
@@ -140,7 +140,7 @@
                     modalContent.innerHTML = `
                         <h2 class="text-2xl font-bold">${data.title}</h2>
                          <div class="flex justify-center p-12">
-                            <img src="${data.image_url}" alt="Blog Image" class="w-64 h-64 object-cover rounded-md border">
+                            <img src="${data.image_url}" alt="Blog Image" class="object-cover w-64 h-64 border rounded-md">
                         </div>
                         <p><strong>Category:</strong> ${data.category}</p>
                         <p class="text-justify" ><strong>Description:</strong> ${data.description}</p>
