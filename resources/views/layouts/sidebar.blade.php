0e9dba3165f7e44adbb65a0c2569b9faccb578e1
Snawlaye <farizfahrian53@gmail.com>
2025-01-20 00:10:41 +0800

    change the icon of export page

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -79,8 +79,11 @@
                 <p class="px-4">Video Profile</p>
             </a>
             <a href="{{route('export')}}" class="{{ request()->is('export*') ? 'active-nav-link' : '' }} flex items-center text-black  py-4 pl-6 nav-item'">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="{{ request()->is('export*') ? '#ffffff' : '' }}" viewBox="0 0 256 256">
+                <!-- <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="{{ request()->is('export*') ? '#ffffff' : '' }}" viewBox="0 0 256 256">
                     <path d="M251.77,73a8,8,0,0,0-8.21.39L208,97.05V72a16,16,0,0,0-16-16H32A16,16,0,0,0,16,72V184a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V159l35.56,23.71A8,8,0,0,0,248,184a8,8,0,0,0,8-8V80A8,8,0,0,0,251.77,73ZM192,184H32V72H192V184Zm48-22.95-32-21.33V116.28L240,95Z"></path>
+                </svg> -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="{{ request()->is('export*') ? '#ffffff' : '' }}" viewBox="0 0 256 256">
+                    <path d="M213.66,66.34l-40-40A8,8,0,0,0,168,24H88A16,16,0,0,0,72,40V56H56A16,16,0,0,0,40,72V216a16,16,0,0,0,16,16H168a16,16,0,0,0,16-16V200h16a16,16,0,0,0,16-16V72A8,8,0,0,0,213.66,66.34ZM168,216H56V72h76.69L168,107.31v84.53c0,.06,0,.11,0,.16s0,.1,0,.16V216Zm32-32H184V104a8,8,0,0,0-2.34-5.66l-40-40A8,8,0,0,0,136,56H88V40h76.69L200,75.31Zm-56-32a8,8,0,0,1-8,8H88a8,8,0,0,1,0-16h48A8,8,0,0,1,144,152Zm0,32a8,8,0,0,1-8,8H88a8,8,0,0,1,0-16h48A8,8,0,0,1,144,184Z"></path>
                 </svg>
                 <p class="px-4">Export Data</p>
             </a>
